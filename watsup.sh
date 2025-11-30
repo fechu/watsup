e83edd76f2cb223cf586740d2198a938daed9a38
Sandro Meier <sandro@sandro-meier.ch>
2025-11-30 21:49:14 +0100

    fix: Spell watsup without "h"

--- watsup.sh
+++ watsup.sh
@@ -1,47 +1,47 @@
 #!/usr/bin/env bash
 
 w() {
-    if [ -z "$WHATSUP_BINARY" ]; then
-        echo "Error: WHATSUP_BINARY is not set."
+    if [ -z "$WATSUP_BINARY" ]; then
+        echo "Error: WATSUP_BINARY is not set."
         return 1
     fi
 
     # Ongoing project, show status and list of possible actions
-	$WHATSUP_BINARY log --current --no-pager --from $(date +%Y-%m-%d) --to $(date +%Y-%m-%d)
+	$WATSUP_BINARY log --current --no-pager --from $(date +%Y-%m-%d) --to $(date +%Y-%m-%d)
     action=$(echo -e "do-nothing\nstart\nstart-edit\nstop\nstop-edit\nabort\nedit\nchange\nhelp" | fzf --height=11 --prompt="Select an action: ")
     case $action in
         start*)
-            project=$($WHATSUP_BINARY projects | fzf --prompt="Select a project to start tracking: ")
+            project=$($WATSUP_BINARY projects | fzf --prompt="Select a project to start tracking: ")
             if [ -n "$project" ]; then
-                $WHATSUP_BINARY start "$project"
+                $WATSUP_BINARY start "$project"
             else
                 echo "No project selected."
             fi
             if [ "$action" = "start-edit" ]; then
-                $WHATSUP_BINARY edit
+                $WATSUP_BINARY edit
             fi
             ;;
         stop)
-            $WHATSUP_BINARY stop
+            $WATSUP_BINARY stop
             echo "Tracking stopped."
             ;;
         stop-edit)
-            $WHATSUP_BINARY stop
-            $WHATSUP_BINARY edit
+            $WATSUP_BINARY stop
+            $WATSUP_BINARY edit
             echo "Tracking stopped."
             ;;
         abort)
-            $WHATSUP_BINARY cancel
+            $WATSUP_BINARY cancel
             echo "Tracking aborted."
             ;;
         edit)
-            $WHATSUP_BINARY edit
+            $WATSUP_BINARY edit
             ;;
         change)
-            $WHATSUP_BINARY stop
-            $WHATSUP_BINARY edit
-            project=$($WHATSUP_BINARY projects | fzf --prompt="Select a project to start tracking: ")
-            $WHATSUP_BINARY start --no-gap "$project"
+            $WATSUP_BINARY stop
+            $WATSUP_BINARY edit
+            project=$($WATSUP_BINARY projects | fzf --prompt="Select a project to start tracking: ")
+            $WATSUP_BINARY start --no-gap "$project"
             ;;
         help)
             echo ""

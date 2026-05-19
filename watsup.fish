7b924ebc7d327d6779797459b2c7977e14a8a423
Sandro Meier <smeier@gravisrobotics.com>
2026-05-19 14:47:31 +0200

    fix: change order elements in fzf when editing via `w`

--- watsup.fish
+++ watsup.fish
@@ -29,7 +29,7 @@ function w
             $WATSUP_BINARY cancel
             echo "Tracking aborted."
         case edit
-            set frame_line ($WATSUP_BINARY log --current --from 2000-01-01 | grep -E '^\s+[0-9a-f]+' | tail -20 | fzf --prompt="Select a frame to edit: ")
+            set frame_line ($WATSUP_BINARY log --current --from 2000-01-01 | grep -E '^\s+[0-9a-f]+' | tail -20 | tac | fzf --prompt="Select a frame to edit: ")
             if test -n "$frame_line"
                 set frame_id (string trim $frame_line | string split -f1 ' ')
                 $WATSUP_BINARY edit $frame_id

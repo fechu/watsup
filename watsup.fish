48b8e55c1e3e564fb424626edcdffced97273170
Sandro Meier <sandro@sandro-meier.ch>
2026-09-11 20:45:39 +0200

    fix: `w edit` with ongoing frame
    If a frame was ongoing and that was picked from the selector with `w
    edit` then there was an error as the current frame's hash does not exist
    in the storage. This is solved by not including the current frame in the
    selection for `w edit` and instead adding `w edit-last`.

--- watsup.fish
+++ watsup.fish
@@ -6,7 +6,7 @@ function w
 
     # Ongoing project, show status and list of possible actions
     $WATSUP_BINARY log --current --from (date +%Y-%m-%d) --to (date +%Y-%m-%d)
-    set action (printf "do-nothing\nstart\nstart-edit\nstop\nstop-edit\nabort\nedit\nchange\nhelp" | fzf --height=11 --prompt="Select an action: ")
+    set action (printf "do-nothing\nstart\nstart-edit\nstop\nstop-edit\nabort\nedit\nedit-last\nchange\nhelp" | fzf --height=12 --prompt="Select an action: ")
     switch $action
         case 'start*'
             set project ($WATSUP_BINARY projects | fzf --prompt="Select a project to start tracking: ")
@@ -29,13 +29,15 @@ function w
             $WATSUP_BINARY cancel
             echo "Tracking aborted."
         case edit
-            set frame_line ($WATSUP_BINARY log --current --from 2000-01-01 | grep -E '^\s+[0-9a-f]+' | tail -20 | tac | fzf --prompt="Select a frame to edit: ")
+            set frame_line ($WATSUP_BINARY log --from 2000-01-01 | grep -E '^\s+[0-9a-f]+' | tail -20 | tac | fzf --prompt="Select a frame to edit: ")
             if test -n "$frame_line"
                 set frame_id (string trim $frame_line | string split -f1 ' ')
                 $WATSUP_BINARY edit $frame_id
             else
                 echo "No frame selected."
             end
+        case edit-last
+            $WATSUP_BINARY edit
         case change
             $WATSUP_BINARY stop
             $WATSUP_BINARY edit
@@ -50,6 +52,14 @@ function w
             echo ""
             echo "Usage:"
             echo "Source this file \"source watsup.fish\" and then run the shortcut \"w\""
+            echo ""
+            echo "Actions:"
+            echo "  start / start-edit       Start tracking a new project"
+            echo "  stop / stop-edit         Stop current tracking"
+            echo "  abort                    Cancel current tracking without saving"
+            echo "  edit                     Edit a past frame"
+            echo "  edit-last                Edit the currently in-progress recording (if any) otherwise the last recording"
+            echo "  change                   Stop current tracking and start tracking a different project"
             echo ""
             echo "Note: This function will mask the w binary (https://man7.org/linux/man-pages/man1/w.1.html)"
         case do-nothing
